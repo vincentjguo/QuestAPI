f7a573c3d47ce660be71d992e8939302f1bea26c
vincentjguo <vincentguo04@gmail.com>
2023-10-13 16:58:28 -0400

    try msedge on path

--- api/scraper/login.py
+++ api/scraper/login.py
@@ -25,9 +25,9 @@ def ini_driver(remember_me):
     else:
         options.add_argument(f"user-data-dir=./profiles/{token}")
     options.add_experimental_option("detach", True)
-    options.binary_location = "./microsoft-edge-stable_117.0.2045.55-1_amd64.deb"
     options.add_argument("--headless")
     options.add_argument("--remote-debugging-port=0")
+    options.add_argument("--no-sandbox")
     s = service.Service(executable_path='api/msedgedriver')
     driver = webdriver.Edge(options=options, service=s)
     common.driver_list[token] = driver
