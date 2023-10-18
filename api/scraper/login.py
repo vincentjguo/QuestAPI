82aa8833d17d2632ab08aff01672e8deed85e780
vincentjguo <vincentguo04@gmail.com>
2023-10-18 16:15:07 -0400

    use disk instead of ram

--- api/scraper/login.py
+++ api/scraper/login.py
@@ -27,6 +27,7 @@ def ini_driver(remember_me):
     options.add_experimental_option("detach", True)
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     """
     options.add_argument("--remote-debugging-port=0")
     options.binary_location = "/usr/bin/microsoft-edge-stable"
